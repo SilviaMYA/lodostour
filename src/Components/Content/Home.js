56ad184e3e18098b8add1f17b208b22f3f69afa1
SilviaMYA <s.mik@hotmail.com>
2019-06-22 14:02:33 +1000

    Components and prop created for Header and Footer and index.js used instead App.js

---
+++ src/Components/Content/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+//import createReactclassName from 'create-react-className';
+import ReactDOM from "react-dom";
+import App from "../../App";
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/App.css'
+
+
+const Home = propos =>  (
+          <main style={{marginTop: '64px'}}>
+              <p className="body_home">
+                  Bodyyyy
+        </p>
+          </main>
+
+      );
+
+
+export default Home;
+
+      
---
+++ src/data/menu.js
@@ -0,0 +1,18 @@
+export default[
+    {
+        title: 'Home',
+        url: '/'
+    },
+    {
+        title: 'Places',
+        url: '/places'
+    },
+    {
+        title: 'About',
+        url: '/about'
+    },
+    {
+        title: 'Contact',
+        url: '/contact'
+    }
+]
